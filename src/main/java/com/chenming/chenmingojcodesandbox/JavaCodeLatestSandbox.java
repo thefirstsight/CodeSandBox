26d3c8427771f006a0367fb137cc6cc87f85174d
chenming <1931517825@qq.com>
2026-06-21 14:49:23 +0800

    commitTest1

--- src/main/java/com/chenming/chenmingojcodesandbox/JavaCodeLatestSandbox.java
+++ src/main/java/com/chenming/chenmingojcodesandbox/JavaCodeLatestSandbox.java
@@ -4,6 +4,9 @@ import com.chenming.chenmingojcodesandbox.model.ExecuteCodeRequest;
 import com.chenming.chenmingojcodesandbox.model.ExecuteCodeResponse;
 import org.springframework.stereotype.Component;
 
+import java.util.ArrayList;
+import java.util.Optional;
+
 /**
  * Java 原生代码沙箱实现
  */
@@ -11,6 +14,7 @@ import org.springframework.stereotype.Component;
 public class JavaCodeLatestSandbox extends JavaCodeSandboxTemplate {
     @Override
     public ExecuteCodeResponse executeCode(ExecuteCodeRequest executeCodeRequest) {
+        ArrayList<Object> objects = new ArrayList<>();
         return super.executeCode(executeCodeRequest);
     }
 }
